2975d7965c57e4be8b1b6d7abe20e5343c0269a3
Juha Kuosa <juha.kuosa@sadeinnovations.com>
2020-04-24 03:28:02 +0300

    first trials

--- src/App.js
+++ src/App.js
@@ -1,26 +1,44 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import logo from './svg/sade_innovations_on_black_background.svg';
 import './App.css';
+import { withAuthenticator } from 'aws-amplify-react'; // or 'aws-amplify-react-native';
+import Amplify from 'aws-amplify';
+import {Auth} from 'aws-amplify';
+import '@aws-amplify/ui/dist/style.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// Get the aws resources configuration parameters
+import awsconfig from './aws-exports'; // if you are using Amplify CLI
+Amplify.configure(awsconfig);
+
+function signOut() {
+  console.log("Signing out");
+  Auth.signOut();
 }
 
-export default App;
+class App extends Component {
+
+  render() {
+    return (
+        <div className="App">
+          <header className="App-header">
+            <a href="http://sadeinnovations.com">
+              <img src={logo} className="App-logo" alt="logo"/>
+            </a>
+          </header>
+
+          <div className="App-body">
+
+          <button
+              onClick={() => signOut()}
+          >
+            Sign out
+          </button>
+
+          </div>
+
+        </div>
+    );
+  }
+}
+
+export default withAuthenticator(App);
